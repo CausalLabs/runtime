71b1d22e0ff0ac5b7fe7b4ece1275363c61f004a
Jeff Palmucci <jeff@causallabs.io>
2022-02-22 10:46:09 -0500

    updated java api

--- src/main/java/io/causallabs/runtime/Requestable.java
+++ src/main/java/io/causallabs/runtime/Requestable.java
@@ -4,65 +4,15 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 
 /**
- * Interface that must be supported for java classes that represent a feature or session impression
+ * Interface that must be supported for java classes that represent a feature in the Java client API
  */
 public interface Requestable {
 
-    /** reset everything to default values */
-    public void reset();
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // Memoization support.
-    //////////////////////////////////////////////////////////////////////////////////
-
-    // Return true if the request should be memoized with the previous request
-    public boolean argsMatch(Requestable obj);
-
-    // Increment the count if another request was just memoized with this one.
-    // optionally pass a new impressionId to keep track of
-    public void incCount(String impressionId);
-
-    public long count();
-
     public String featureName();
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // Serialization Support
-    //////////////////////////////////////////////////////////////////////////////////
-
-    // serialize the arguments to be sent to the impression server. gen should
-    // already be in the object context
-    public void serializeArgs(JsonGenerator gen);
-
-    // Deserialize the arguments from a client into this object
-    public void deserializeArgs(JsonParser next) throws ApiException;
-
-    // Serialize the result to be sent to the client
-    public void serializeResponse(JsonGenerator gen);
-
-    // Take a response from the server and deserialize it into this object.
-    // ApiExceptions can happen if we try to deserialize a value to somewhere
-    // it can't go. IE deserializing a string to an int.
-    public void deserializeResponse(JsonParser parser) throws ApiException;
-
-    //////////////////////////////////////////////////////////////////////////////////
-    // avro style access to fields
-    //////////////////////////////////////////////////////////////////////////////////
-
-    public void put(int i, Object o);
-
-    public boolean isSet(int i);
-
-    public Object get(int i);
-
-    // set the given output as an external value with the given name
-    public void putExternal(int i, String name);
 
     ////////////////////////////////////////////////////////////////////////////////
     // Support for feature gating
     ////////////////////////////////////////////////////////////////////////////////
-    public void setActive(boolean b);
-
     /**
      * Is this feature active?
      * 
@@ -73,7 +23,6 @@ public interface Requestable {
     ////////////////////////////////////////////////////////////////////////////////
     // Support for recoveralbe errors
     ////////////////////////////////////////////////////////////////////////////////
-    public void setError(Throwable t);
 
     /**
      * Check if a recoverable error occurred
@@ -82,12 +31,23 @@ public interface Requestable {
      */
     public Throwable getError();
 
-    ////////////////////////////////////////////////////////////////////////////////
-    // For quick access from native code
-    ////////////////////////////////////////////////////////////////////////////////
-
     public String getDeviceId();
 
     public void setDeviceId(String x);
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // For use by causal runtinme
+    ////////////////////////////////////////////////////////////////////////////////
+    void setActive(boolean b);
+
+    void setError(Throwable t);
+
+    // serialize the arguments to be sent to the impression server. gen should
+    // already be in the object context
+    void serializeArgs(JsonGenerator gen);
+
+    // Take a response from the server and deserialize it into this object.
+    // ApiExceptions can happen if we try to deserialize a value to somewhere
+    // it can't go. IE deserializing a string to an int.
+    void deserializeResponse(JsonParser parser) throws ApiException;
 }
---
+++ src/main/java/io/causallabs/runtime/Requested.java
@@ -0,0 +1,70 @@
+package io.causallabs.runtime;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+
+/**
+ * Interface that must be supported for java classes that represent a feature inside the impression
+ * server
+ */
+public interface Requested {
+
+    /** reset everything to default values */
+    public void reset();
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // Memoization support.
+    //////////////////////////////////////////////////////////////////////////////////
+
+    // Return true if the request should be memoized with the previous request
+    public boolean argsMatch(Requested obj);
+
+    // Increment the count if another request was just memoized with this one.
+    // optionally pass a new impressionId to keep track of
+    public void incCount(String impressionId);
+
+    public long count();
+
+    public String featureName();
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // Serialization Support
+    //////////////////////////////////////////////////////////////////////////////////
+
+    // Deserialize the arguments from a client into this object
+    public void deserializeArgs(JsonParser next) throws ApiException;
+
+    // Serialize the result to be sent to the client
+    public void serializeResponse(JsonGenerator gen);
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // avro style access to fields
+    //////////////////////////////////////////////////////////////////////////////////
+
+    public void put(int i, Object o);
+
+    public boolean isSet(int i);
+
+    public Object get(int i);
+
+    // set the given output as an external value with the given name
+    public void putExternal(int i, String name);
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Support for feature gating
+    ////////////////////////////////////////////////////////////////////////////////
+    public void setActive(boolean b);
+
+    /**
+     * Is this feature active?
+     * 
+     * @return false if gated off
+     */
+    public boolean isActive();
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // For quick access from native code
+    ////////////////////////////////////////////////////////////////////////////////
+
+    public String getDeviceId();
+}
