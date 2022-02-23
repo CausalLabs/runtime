46785abb184ba0f47071a962a96e2e974764a0b1
Jeff Palmucci <jeff@causallabs.io>
2022-02-23 09:14:28 -0500

    Support for mutable session fields

---
+++ src/main/java/io/causallabs/runtime/Mutable.java
@@ -0,0 +1,64 @@
+package io.causallabs.runtime;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.IndexedRecord;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mutable<T> {
+
+    public void setValue(T x) {
+        long now = System.currentTimeMillis();
+        if (!values.isEmpty()) {
+            values.get(values.size() - 1).endTime = now - 1;
+        }
+        values.add(new Record(now, x));
+    }
+
+    public T getValue() {
+        return values.get(values.size() - 1).value;
+    }
+
+    public List<Record> getHistory() {
+        if (!values.isEmpty()) {
+            values.get(values.size() - 1).endTime = System.currentTimeMillis() - 1;
+        }
+        return values;
+    }
+
+    public class Record implements IndexedRecord {
+        public Record(long now, T x) {
+            startTime = now;
+            value = x;
+        }
+
+        long startTime, endTime;
+        T value;
+
+        @Override
+        public Schema getSchema() {
+            // schema is generated in the iserver
+            return null;
+        }
+
+        @Override
+        public void put(int i, Object v) {
+            // we don't read these in
+        }
+
+        @Override
+        public Object get(int i) {
+            switch (i) {
+                case 0:
+                    return startTime;
+                case 1:
+                    return endTime;
+                case 2:
+                    return value;
+            }
+            return null;
+        }
+    }
+
+    ArrayList<Record> values = new ArrayList<>();
+}
