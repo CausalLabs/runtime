e75594e249c40b88de9fd96191ae1a77725c741e
Alex May <alex@causallabs.io>
2022-12-16 11:09:54 -0500

    WIP: more template fields

--- src/main/java/io/causallabs/mustache/WarehouseColumn.java
+++ src/main/java/io/causallabs/mustache/WarehouseColumn.java
@@ -27,9 +27,32 @@ public interface WarehouseColumn {
     /** returns true if it is a normal output, false if it is a plugin output or an argument */
     public boolean isOutput();
 
+    /**
+     * returns true if this field is marked with @per for use in a metric (only applicable to
+     * session fields)
+     */
+    public boolean isPer();
+
+    /* return true if this field is marked with @session_key (only applicable to session fields) */
+    public boolean isSessionKey();
+
+    /* return true if this field is marked with @split_key (only applicable to session fields) */
+    public boolean isSplitKey();
+
+    /* returns true if this field is the persistent key (only applicable to session fields) */
+    public boolean isPersistentKey();
+
+
     /** returns the FDL filenam and line number where this entitity was defined */
     public FileLocation getFileLocation();
 
     /** returns the feature that this was defined on */
     public String getFeatureName();
+
+    public enum ElapsedType {
+        SESSION_END
+    };
+
+    /** returns the elapsed type (@elapsed directive). Null if none */
+    public ElapsedType getElapsedType();
 }
