9a77939ff2589478766f194226070059a12e6fac
Alex May <alex@causallabs.io>
2023-01-01 12:55:17 -0500

    fix javadoc

--- src/main/java/io/causallabs/mustache/package-info.java
+++ src/main/java/io/causallabs/mustache/package-info.java
@@ -2,11 +2,11 @@
  * This package defines the context made available to the JMustache template processor when
  * expanding user-defined mustache template with the --template causalc command line option.
  * <p>
- * The context includes: <ui>
+ * The context includes: <ul>
  * <li>session - points to the session {@link io.causallabs.mustache.Feature}</li>
  * <li>features - points to the list of {@link io.causallabs.mustache.Feature}s</li>
  * <li>events - points to the list of abstract {@link io.causallabs.mustache.AbstractEvent}s</li>
- * </ui>
+ * </ul>
  * <p>
  * In addition the values stored in the json file specified using the --template-params argument. So
  * if you have a params file as follows:
