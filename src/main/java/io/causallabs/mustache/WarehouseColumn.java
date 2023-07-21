b36f45d6a87a47ba6b983b2462b1ea6b6009f6d9
Louis Calisi <louis@causallabs.io>
2023-07-21 17:40:25 -0400

    Offline events tables are created.

--- src/main/java/io/causallabs/mustache/WarehouseColumn.java
+++ src/main/java/io/causallabs/mustache/WarehouseColumn.java
@@ -1,26 +1,8 @@
 package io.causallabs.mustache;
 
 /** Represents a field (argument, output, or event) of a feature's warehouse table */
-public interface WarehouseColumn {
+public interface WarehouseColumn extends Field {
 
   /** The name of the table this field is stored in */
   public String getTableName();
-
-  /** Object that represents the comments and deprecation status of the field */
-  public Description getDescription();
-
-  /** The name of the column this field is stored in. The FDL field name, but in snake_case. */
-  public String getColumnName();
-
-  /** The ORC schema definition */
-  public String getOrcType();
-
-  /** The Redshift data type when this field is exposed as a Redshift column */
-  public String getRedshiftType();
-
-  /** The Redshift Spectrum data type when this field is exposed as a Redshift column */
-  public String getSpectrumType();
-
-  /** The Snowflake data type. */
-  public String getSnowflakeType();
 }
