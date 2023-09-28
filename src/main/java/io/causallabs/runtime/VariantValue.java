72c72bb80073dbdc6497a14f92109706a69b5ed9
evancausal <evan@causallabs.io>
2023-09-28 18:18:58 -0400

    Add interface type to variant value

--- src/main/java/io/causallabs/runtime/VariantValue.java
+++ src/main/java/io/causallabs/runtime/VariantValue.java
@@ -2,13 +2,16 @@ package io.causallabs.runtime;
 
 public class VariantValue {
 
-  public VariantValue(String featureName, String featureField, Object featureValue) {
+  public VariantValue(
+      String featureName, String featureField, Object featureValue, String interfaceType) {
     this.featureName = featureName;
     this.featureField = featureField;
     this.featureValue = featureValue;
+    this.interfaceType = interfaceType;
   }
 
   public final String featureName;
   public final String featureField;
   public Object featureValue;
+  public final String interfaceType;
 }
