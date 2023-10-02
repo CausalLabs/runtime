fcd93fa3fd96559216869c0db51c49ff350a6bb7
evancausal <evan@causallabs.io>
2023-10-02 10:04:46 -0400

    Add target value

--- src/main/java/io/causallabs/runtime/VariantValue.java
+++ src/main/java/io/causallabs/runtime/VariantValue.java
@@ -3,15 +3,21 @@ package io.causallabs.runtime;
 public class VariantValue {
 
   public VariantValue(
-      String featureName, String featureField, Object featureValue, String interfaceType) {
+      String featureName,
+      String featureField,
+      Object featureValue,
+      String interfaceType,
+      String targetValue) {
     this.featureName = featureName;
     this.featureField = featureField;
     this.featureValue = featureValue;
     this.interfaceType = interfaceType;
+    this.targetValue = targetValue;
   }
 
   public final String featureName;
   public final String featureField;
   public Object featureValue;
   public final String interfaceType;
+  public final String targetValue;
 }
